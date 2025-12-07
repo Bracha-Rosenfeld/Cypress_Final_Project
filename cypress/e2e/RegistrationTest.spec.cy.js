6f5e75866251a6ba90150a0d6dfb28f44a3cd38a
Bracha-Rosenfeld <Hand4d28@mbjcomp.org.il>
2025-12-07 08:12:35 +0200

    added another test for invalid input - the test now checks if the email is OK and did that the previous one checks if the phone number is valid

--- cypress/e2e/RegistrationTest.spec.cy.js
+++ cypress/e2e/RegistrationTest.spec.cy.js
@@ -2,14 +2,28 @@ describe("Registration Tests", () => {
 
     const url = "https://www.bodyshop.co.il/%D7%94%D7%A6%D7%98%D7%A8%D7%A4%D7%95%D7%AA-%D7%9C%D7%9E%D7%95%D7%A2%D7%93%D7%95%D7%9F/";
 
-    it("invalid data", () => {
+    it("invalid data 1", () => {
 
         cy.visit(url);
-        cy.get("#form-field-name").type("Bracha123");
-        cy.get("#form-field-field_60edae3").type("Rosen45");
+        cy.get("#form-field-name").type("Bracha");
+        cy.get("#form-field-field_60edae3").type("Rose");
+        cy.get("#form-field-field_579f0e8-1").click();
+        cy.get("#form-field-email").type("05");
+        cy.get("#form-field-field_f19d3d6").type("bracha@example.com");
+        cy.get("#form-field-field_801783e").click({ force: true });
+        cy.get("#form-field-field_2e50be6").click({ force: true });
+
+        cy.get(".elementor-button.elementor-size-sm").should("not.be.enabled");
+    });
+
+    it("invalid data 2", () => {
+
+        cy.visit(url);
+        cy.get("#form-field-name").type("Bracha");
+        cy.get("#form-field-field_60edae3").type("Rose");
         cy.get("#form-field-field_579f0e8-1").click();
         cy.get("#form-field-email").type("0500000000");
-        cy.get("#form-field-field_f19d3d6").type("not-an-email");
+        cy.get("#form-field-field_f19d3d6").type("bracha@g.");
         cy.get("#form-field-field_801783e").click({ force: true });
         cy.get("#form-field-field_2e50be6").click({ force: true });
 
